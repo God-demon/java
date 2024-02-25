df8496962ec7ad798345e7bf21298a58bb8e4d67
noob <opnoob955@gmail.com>
2024-02-25 17:24:31 +0530

    First Compile this code Then use it  ;)

--- console_color.java
+++ console_color.java
@@ -75,3 +75,6 @@ public class console_color {
     public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
     public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
 }
+
+
+// This is color for console
